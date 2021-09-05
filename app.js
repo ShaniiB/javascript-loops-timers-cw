c6849f54c4afa1dff7872c4e1d0fe31c53302387
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-09-05 13:27:51 -0700

    completed guy animation

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('Sanity Test');
+
+// Find the Largest Number in the Array
+// Write a JavaScript conditional statement to find the largest of the five numbers from the array below. Use an alert box to show the result.
+
+// ```const myNumberList = [12, 2112, 9000, 1999, 24];```
+
+// ==================================================================================================== //
+
+
+// Exercise 1: While/For Loops
+// - Create an empty `name_array`
+// - Ask the user ```Enter a word. Enter 'q' to quit.``` Add each word they enter to the array
+// - Create a while loop that will keep asking for words to add to the array until they enter 'q'
+// - When the user enters 'q' use a **for** loop to print each word from the array
+// - NOTE: Your code should handle the case of the entered quite letter `Q`
+
+// create array to store new words from the user. 
+// let newWord = [];
+// prompt(`Enter a new word\nPress 'q' to quit.`); 
+
+// {
+//     console.log(`[${word}]`);
+// }
---
+++ guy.js
@@ -0,0 +1,3 @@
+console.log('Wii');
+// Timer Challenge 1:
+// Animate the included sprite images of 'guy' images. Start with an initial image of `guy1.png` then every .5 second (500 ms) update the image to the next one in sequence. Once you reach image `guy9.png` start over with the `guy1.png` image.
