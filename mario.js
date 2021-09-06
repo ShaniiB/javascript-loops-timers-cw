b0cbc46fe27b4efee058658d88cb5619e75cf653
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-09-05 23:35:44 -0700

    mario

---
+++ mario.js
@@ -0,0 +1,6 @@
+console.log('');
+
+// SUPER MARIO Challenge 2:
+// Animate Mario walking to the left and right by pressing the left and right arrow keys (HINT: implement event handler(s) for handling the key //presses)
+
+
